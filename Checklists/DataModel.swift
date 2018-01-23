6e5d6813db48167f79a47e9969d444d42e0cd67e
Alex Loo <alexander.loo@gmail.com>
2018-01-23 15:46:54 -0800

    added default list for first-run experience. ended on page 204.

--- Checklists/DataModel.swift
+++ Checklists/DataModel.swift
@@ -14,6 +14,7 @@ class DataModel {
     init() {
         loadChecklists()
         registerDefaults()
+        handleIfFirstTime()
     }
     
     func documentsDirectory() -> String{
@@ -46,9 +47,22 @@ class DataModel {
     }
     
     func registerDefaults() {
-        let dictionary = [ "ChecklistIndex": -1 ]
+        let dictionary: [ String: Any ] = [ "ChecklistIndex": -1, "FirstTime": true ]
         
         UserDefaults.standard.register(defaults: dictionary)
+    }
+    
+    func handleIfFirstTime() {
+        let userDefaults = UserDefaults.standard
+        let firstTime = userDefaults.bool(forKey: "FirstTime")
+        
+        if firstTime {
+            let checklist = Checklist(name: "List")
+            lists.append(checklist)
+            indexOfSelectedChecklist = 0
+            userDefaults.set(false, forKey: "FirstTime")
+            userDefaults.synchronize()
+        }
     }
     
     var indexOfSelectedChecklist: Int {
